e48c0b29a901828df895fae8b11d9c047016305e
Adnan <adnantabda@gmail.com>
2025-05-11 23:33:52 +0300

    chore: update vite config to copy manifest and icon assets

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,31 @@
-export default {
-    build: {
-      rollupOptions: {
-        input: {
-          content: 'src/content.js'
-        },
-        output: {
-          entryFileNames: '[name].js'
-        }
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',  // Output directory for the build
+    emptyOutDir: true,  // Clean the dist folder before building
+    rollupOptions: {
+      input: {
+        content: 'src/content.js',  // Your main JS file
       },
-      outDir: 'dist',
-      emptyOutDir: true,
-    }
-  };
-  
+      output: {
+        entryFileNames: '[name].js',  // Output JS file naming
+      },
+    },
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/highlight.css',  // Copy the CSS file
+          dest: 'assets',  // Copy to 'dist/assets' folder
+        },
+        {
+          src: 'public/icons/*',  // Copy icons
+          dest: 'icon',  // Copy to 'dist/icon' folder
+        },
+      ],
+    }),
+  ],
+});
