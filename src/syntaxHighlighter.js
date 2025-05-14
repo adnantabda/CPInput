6b3ef32c3820a97c5aadaf342eafedb2529b3065
Adnan <adnantabda@gmail.com>
2025-05-14 22:42:59 +0300

    feat: add C++ support for code highlighting
    - Registered 'cpp' language  using hljs.registerLanguage
    - Enables C++ syntax highlighting in supported components

--- src/syntaxHighlighter.js
+++ src/syntaxHighlighter.js
@@ -1,11 +1,13 @@
 import hljs from 'highlight.js/lib/core';
 import python from 'highlight.js/lib/languages/python';
 import javascript from 'highlight.js/lib/languages/javascript';
+import cpp from 'highlight.js/lib/languages/cpp';
 // Add other languages as needed
 
 // Register languages
 hljs.registerLanguage('python', python);
 hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('cpp', cpp)
 // Register other languages as needed
 
 export function highlightCode(code, language) {
