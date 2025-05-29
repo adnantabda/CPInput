2b621087cd82c6259d2abdd1583c83d09d1811ff
Adnan <adnantabda@gmail.com>
2025-05-29 14:24:32 +0300

    Add language support to syntaxHighlighter for remaining competitive programming languages

--- src/syntaxHighlighter.js
+++ src/syntaxHighlighter.js
@@ -2,12 +2,27 @@ import hljs from 'highlight.js/lib/core';
 import python from 'highlight.js/lib/languages/python';
 import javascript from 'highlight.js/lib/languages/javascript';
 import cpp from 'highlight.js/lib/languages/cpp';
+import kotlin from 'highlight.js/lib/languages/kotlin'
+import java from 'highlight.js/lib/languages/java'
+import go from 'highlight.js/lib/languages/go'
+import rust from 'highlight.js/lib/languages/rust'
+import csharp from 'highlight.js/lib/languages/csharp'
+
+
+
 // Add other languages as needed
 
 // Register languages
 hljs.registerLanguage('python', python);
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('cpp', cpp)
+hljs.registerLanguage('kotlin', kotlin)
+hljs.registerLanguage('csharp', csharp)
+hljs.registerLanguage('java', java)
+hljs.registerLanguage('go', go)
+hljs.registerLanguage('rust', rust)
+
+
 // Register other languages as needed
 
 export function highlightCode(code, language) {
